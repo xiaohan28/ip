535e6583462acd8e137c67dc5e39be8d5c25e458
xiaohan28 <zgxiaohan0628@gmail.com>
2025-01-22 16:22:45 +0800

    Replaced Duke.java with Eepy.java; complete level-0

---
+++ src/main/java/Eepy.java
@@ -0,0 +1,12 @@
+package ip.src.main.java;
+
+public class Eepy {
+    public static void main(String[] args) {
+        System.out.println("_____________________________________");
+        System.out.println("Hello! I'm Eepy");
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________________");
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println("_____________________________________");
+    }
+}
