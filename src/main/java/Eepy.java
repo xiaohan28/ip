dcc00012123552cccc55a84e5441447e164bccfb
xiaohan28 <zgxiaohan0628@gmail.com>
2025-02-01 21:12:02 +0800

    Changed Echo to To-Do Tracker

--- src/main/java/Eepy.java
+++ src/main/java/Eepy.java
@@ -3,23 +3,35 @@ import java.util.Scanner;
 
 public class Eepy {
 
-    public static void echo (String[] args){
-        System.out.println("Start Echo");
+    public static void toDoTracker (String[] args) {
+        System.out.println("_____________________________________");
+        System.out.println("Start To-Do List Tracker");
+        System.out.println("_____________________________________");
         Scanner input = new Scanner(System.in);
-        String userInput = input.nextLine();
+        String[] userInputArray = new String[100];
+        int inputCounter = 0;
+        String userInput = input.nextLine(); //initial input
         System.out.println("_____________________________________");
         while (!userInput.equals("bye")) {
-            System.out.println("    Echo: " + userInput);
+            if (userInput.equals("list")) { //if "list" command, list the elements in the array
+                for (int i = 0; i < inputCounter; i++) {
+                    System.out.println((i + 1) + ":" + userInputArray[i]);
+                }
+            } else { //else add user input to the array and echo the input
+                System.out.println("    Added: " + userInput);
+                userInputArray[inputCounter] = userInput;
+                inputCounter++;
+            } //continue accepting input after 'list' or 'add'
             System.out.println("_____________________________________");
             userInput = input.nextLine();
             System.out.println("_____________________________________");
         }
-        System.out.println("End of Echo, bye! Hope to see you again soon :>");
+        System.out.println("End of To-Do Tracker, bye! Hope to see you again soon :>");
         System.out.println("_____________________________________");
     }
     public static void main(String[] args) {
         System.out.println("_____________________________________");
         System.out.println("Hello! I'm Eepy" + "\n" + "What can I do for you?");
-        echo(args);
+        toDoTracker(args);
     }
 }
