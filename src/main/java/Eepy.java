05e55d183ce0f8bf44862be144e97b66364e9605
xiaohan28 <zgxiaohan0628@gmail.com>
2025-02-01 20:17:09 +0800

    Add echo user's command function

--- src/main/java/Eepy.java
+++ src/main/java/Eepy.java
@@ -1,12 +1,25 @@
 package ip.src.main.java;
+import java.util.Scanner;
 
 public class Eepy {
+
+    public static void echo (String[] args){
+        System.out.println("Start Echo");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        System.out.println("_____________________________________");
+        while (!userInput.equals("bye")) {
+            System.out.println("    Echo: " + userInput);
+            System.out.println("_____________________________________");
+            userInput = input.nextLine();
+            System.out.println("_____________________________________");
+        }
+        System.out.println("End of Echo, bye! Hope to see you again soon :>");
+        System.out.println("_____________________________________");
+    }
     public static void main(String[] args) {
         System.out.println("_____________________________________");
-        System.out.println("Hello! I'm Eepy");
-        System.out.println("What can I do for you?");
-        System.out.println("_____________________________________");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("_____________________________________");
+        System.out.println("Hello! I'm Eepy" + "\n" + "What can I do for you?");
+        echo(args);
     }
 }
